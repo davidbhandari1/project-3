14c08da95ca0447b10bcb65073813cfa4f2bb9d6
davidbhandari1 <davidbhandari2055@gmail.com>
2022-12-19 15:02:12 +0545

    A new document is added.

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(requests):
     return render(requests, 'index.html')
@@ -8,7 +8,22 @@ def about(requests):
     return render(requests, 'about.html')
 
 def contact(requests):
-    return render(requests, 'contact.html')
+    if requests.method =='POST':
+        name = requests.POST['name']
+        email = requests.POST['email']
+        subject = requests.POST['subject']
+        message = requests.POST['message']
+        data = Contact.objects.create(
+            name = name,
+            email = email,
+            subject = subject,
+            message = message
+        )
+        data.save()
+        message ={'mess':'the message is submitted'}
+        return render(requests, 'contact.html',message )
+
+    return render(requests, 'contact.html', )
 
 def portfolio(requests):
     return render(requests, 'portfolio.html')
